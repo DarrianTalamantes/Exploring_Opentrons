39821b1e2651f672182c60d1f3c39369ed5a6496
DarrianTalamantes <darrianrtalamantes6@gmail.com>
2021-11-29 12:15:43 -0800

    Made the qpcr small set protocol work with up to 48 samples

--- array_visualizer.py
+++ array_visualizer.py
@@ -1,10 +1,11 @@
 import math
 import json
 import numpy as np
-
-plate_array = np.zeros((4, 6), dtype='U25')
+a = 23 // 6
+print(a)
+plate_array = np.zeros((14, 21), dtype='U25')
 alphabate = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
-for col in range(0, 6):
-    for row in range(0, 4):
-        plate_array[row, col] = alphabate[row] + str(col + 1)
+for col in range(0, 21):
+    for row in range(0, 14):
+        plate_array[row, col] = alphabate[row + 1] + str(col + 2)
 print(plate_array)
